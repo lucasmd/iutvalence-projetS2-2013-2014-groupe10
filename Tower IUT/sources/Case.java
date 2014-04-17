89460acb56aa9027affd676f904a7d307bdc8c55
LucasMD <LucasMD@192.168.100.103>
2014-04-17 15:40:15 +0200

    Ajout de la javadoc pour les classes Case et Map

--- Tower IUT/sources/Case.java
+++ Tower IUT/sources/Case.java
@@ -1,15 +1,25 @@
 
+/**
+ * Création de la classe Case
+ *
+ */
 public class Case
 {
 	private boolean casePleine;
 	
 	
+	/**
+	 * constructeur de case, par défault false (vide)
+	 */
 	public Case()
 	{
 		this.casePleine = false;
 	}
 
-	 public boolean obtenirContenu()
+	 /**
+	 * methode permettant de renvoyer l'état d'une case, vide ou pleine (booléen)
+	 */
+	public boolean obtenirContenu()
 	 {
 		 if (this.casePleine==false)
 			 return true;
--- Tower IUT/sources/Map.java
+++ Tower IUT/sources/Map.java
@@ -1,29 +1,49 @@
 
 
+/**
+ * Creation de la classe Map 
+ */
 public class Map
 {
 	 private final Case[][] map;
 	
-	 public Map(){
+	 /**
+	 * Création du constructeur permettant d'initialiser la Map avec une taille de 30/30
+	 */
+	public Map(){
 		 map = new Case[30][30];
 	 }
 	 
-	 public void afficherMap()
+	 /**
+	 * méthode permettant d'afficher la map
+	 */
+	public void afficherMap()
 	 {
 		 
 	 }
 	 
-	 public void miseAJourPlateau()
+	 /**
+	 * méthode permettant de faire une mise a jour du plateau
+	 */
+	public void miseAJourPlateau()
 	 {
 		 
 	 }
 	 
-	 public void afficherEnnemi(Ennemi ennemi)
+	 /**
+	 * méthode permettant d'afficher l'ennemi
+	 * @param ennemi
+	 */
+	public void afficherEnnemi(Ennemi ennemi)
 	 {
 		 
 	 }
 	 
-	 public void afficherTour(Tour tour)
+	 /**
+	 * méthode permettant d'afficher la tour
+	 * @param tour
+	 */
+	public void afficherTour(Tour tour)
 	 {
 		 
 	 }
