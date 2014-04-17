a5f83ddca1a27e82280980c2246dd528c5bb2b70
prinsacn <nathan.prinsac@iut-valence.fr>
2014-04-17 14:58:41 +0200

    Ajout des classes Position et Tour avec les attributs, les constructeurs et les méthodes (vides). Fait par Alexandre et Nathan.

---
+++ Tower IUT/sources/Position.java
@@ -0,0 +1,43 @@
+/**
+ * Donne la position d'une case ou d'une tour
+ * 
+ * @author prinsacn
+ * 
+ */
+
+public class Position {
+
+	/**
+	 * Numéro de la ligne
+	 */
+	private int posiLigne;
+	/**
+	 * Numéro de la colonne
+	 */
+	private int posiColonne;
+
+	/**
+	 * Permet d'initialiser une position
+	 * 
+	 * @param posiLigne
+	 * @param posiColonne
+	 */
+	public Position(int posiLigne, int posiColonne) {
+		this.posiLigne = posiLigne;
+		this.posiColonne = posiColonne;
+	}
+
+	/**
+	 * Retourne le numéro de colonne
+	 */
+	public int obtenirPositionColonne() {
+		return this.posiColonne;
+	}
+
+	/**
+	 * Retourne le numéro de ligne
+	 */
+	public int obtenirPositionLigne() {
+		return this.posiLigne;
+	}
+}
---
+++ Tower IUT/sources/Tour.java
@@ -0,0 +1,63 @@
+/**
+ * Définition d'une tour
+ * @author prinsacn
+ *
+ */
+
+public class Tour {
+
+	private int taille;
+	private int perimettreAttaque;
+	private int prix;
+	private int puissanceAttaque;
+	private int pointDegats;
+	
+	
+	
+	public Tour(int typeTour)
+	{
+		if (typeTour==1){
+			this.taille=;//à décider
+			this.perimettreAttaque=;//à décider
+			this.prix=;//à décider
+			this.puissanceAttaque=;//à décider
+			this.pointDegats=;//à décider
+		}
+		else if (typeTour==2){
+			this.taille=;//à décider
+			this.perimettreAttaque=;//à décider
+			this.prix=;//à décider
+			this.puissanceAttaque=;//à décider
+			this.pointDegats=;//à décider
+		}
+		else if (typeTour==3){
+			this.taille=;//à décider
+			this.perimettreAttaque=;//à décider
+			this.prix=;//à décider
+			this.puissanceAttaque=;//à décider
+			this.pointDegats=;//à décider
+		}
+	}
+	
+	public void poserTour()
+	{
+		
+	}
+	
+	public void suppressionTour()
+	{
+		
+	}
+	
+	public void attaquer()
+	{
+		
+	}
+	
+	public void creerTour(int typeTour)
+	{
+		
+	}
+	
+}	
+
