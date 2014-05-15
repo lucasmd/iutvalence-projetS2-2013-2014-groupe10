37b5306c5735ebbe81a1436ec405655173b27c9f
Heloise-fernandes <heloies.fernandesdealmeida@iut-valence.fr>
2014-05-15 14:30:33 +0200

    test3

--- Tower IUT/sources/Map.java
+++ Tower IUT/sources/Map.java
@@ -1,40 +1,41 @@
 public class Map
 {
 	/** Tableau de case qui forme la map. */
-	 private final Case[][] map;
+	private final static int NOMBRE_COLONNE=30;
+	private final static int NOMBRE_LIGNE=30;
+	private Case[][] map ;
 	
 	 /** Constructeur permettant d'initialiser la Map avec une taille de 50/50 */
-	public Map(){
-		 map = new Case[50][50];
-	 }
+	public Map()
+	{
+		map= new Case[NOMBRE_LIGNE][NOMBRE_COLONNE]; 
+		for(int colonne=0; colonne<NOMBRE_COLONNE; colonne++)
+		 { 
+			for(int ligne=0; ligne<NOMBRE_LIGNE; ligne++)
+			{
+				map[ligne][colonne]=new Case(ligne,colonne);
+			}
+		 }
+	}
+	
 	 
 	 /** Retourne graphiquement la map (IHM). */
 	public void afficherMap()
 	 {
-		 
+		for(int colonne=0; colonne<NOMBRE_COLONNE; colonne++)
+		 { 
+			for(int ligne=0; ligne<NOMBRE_LIGNE; ligne++)
+			{
+				System.out.print(map[ligne][colonne]);
+			}
+			System.out.println("\n");
+		 }
 	 }
 	 
-	 /** Met Ã  jour le plateau. */
+	 /** Met à jour le plateau. */
 	public void miseAJourPlateau()
 	 {
 		 
 	 }
-	 
-	 /**
-	 * Retourne graphiquement les ennemis (IHM).
-	 * @param ennemi
-	 */
-	public void afficherEnnemi(Ennemi ennemi)
-	 {
-		 
-	 }
-	 
-	 /**
-	 * Retourne graphiquement les tours (IHM).
-	 * @param tour
-	 */
-	public void afficherTour(Tour tour)
-	 {
-		 
-	 }
+	
 }
