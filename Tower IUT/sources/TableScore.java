2c2cb3ddbfcf6a99defe90fc43953d997218e960
Alexandre <Alexandre@PC-ALEX>
2014-05-15 15:58:19 +0200

    Ajout de la classe TableScore

---
+++ Tower IUT/sources/TableScore.java
@@ -0,0 +1,20 @@
+import java.io. *; 
+
+public class TableScore {
+
+	public TableScore(){
+		
+	}
+	
+	public int afficherScore(){
+		
+	}
+	
+	public void fermerScore(){
+		
+	}
+	
+	public void modifierScore(int nouveauScore){
+		
+	}
+}
